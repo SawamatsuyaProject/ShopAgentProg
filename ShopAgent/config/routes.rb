d1383e53f805079a34da8dae5766fb673101ca94
高松瞳 <takamatsu_hitomi@takamatsuhitominoMacBook.local>
2018-04-24 22:27:32 +0900

    ホーム・FAQ・申し込みフォームを作成

--- ShopAgent/config/routes.rb
+++ ShopAgent/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/top'
+
+  get 'faq/top'
+
+  resources :orders
+
+  root to: 'home#top', as: 'home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
