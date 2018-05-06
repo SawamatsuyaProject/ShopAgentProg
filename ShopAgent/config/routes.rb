255557564d37c5bbb97a8a0e005c0c35b0a208b3
takamatsu-hitomi <cl9u9cl3@gmail.com>
2018-05-06 15:21:06 +0900

    お問い合わせフォームの作成　メアドやパスワードなど設定すればメール送信も可能

--- ShopAgent/config/routes.rb
+++ ShopAgent/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'inquiry/index'
+
+  get 'inquiry/confirm'
+
+  get 'inquiry/thanks'
+
   get 'use_sample/top'
 
   get 'home/top'
@@ -8,6 +14,10 @@ Rails.application.routes.draw do
   resources :orders
 
   resources :posts
+
+  get 'inquiry' => 'inquiry#index'              # 入力画面
+  post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post 'inquiry/thanks' => 'inquiry#thanks'     # 送信完了画面
 
   root to: 'home#top', as: 'home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
